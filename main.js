8da059f400e92f4f8af36d8342237040885dfc3b
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-20 19:42:28 -0800

    index

--- main.js
+++ main.js
@@ -1,51 +1,16 @@
-/*let fruit=["apple","orange","mango","bannana","lemon"];
-console.log(fruit);
 
-let delArray=[];
-
-let b=0;
-let a =parseInt(prompt("enter the name"));
-let j=fruit[a];
-
-for(i=0;i<fruit.length; i++){
-
-  if(fruit[i] != j){
-    delArray[b]=fruit[i]
-    b++;
-  }
-}
-
-
-console.log(delArray)
-
-
-for(i=0; i<=a; i++){
-
-  fruit[i]=c
-
-  function price(num) {
-  let num=
-  let c=1000;
-  for(num=0; num<=c; num++){
-
-  num>= 500;
-  return  document.write(num)
-  }
-  
-}
-
-      
-      
-}*/
-
-let num=[800,500,600,700,100,200,1000,1200,1300]
+/*let num=[800,500,600,700,100,200,1000,1200,1300];
 
 function price(num) {
-
   return  (num>= 500 && 1000>=num);
 }
-
 document.write(num.filter(price))
+*/
 
+let price=[800,500,600,700,100,200,1000,1200,1300];
+let result=price.filter(function (price){
+  return  (price>= 500 && 1000>=price);
+});
 
+document.write(result)
 
